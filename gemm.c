a42305fe29cc6ea2ee28da672ae7f16c29b569a9
Omri Shneor <omrishneor@gmail.com>
2022-11-05 17:48:22 +0200

    Hopefully fixing some bugz

--- gemm.c
+++ gemm.c
@@ -1,11 +1,28 @@
-#define N 1024
+#include <stdlib.h>
+#include <stdio.h>
+#define N 16
 
 float A[N][N];
 float B[N][N];
 float C[N][N];
 
+void mat_mul(float A[N][N], float B[N][N]){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++) {
+            for(int k = 0; k < N; k++) {
+                C[i][j] += A[i][k]*B[k][j];
+            }
+            printf("%f ", C[i][j]);
+        } 
+    }
+}
+
+
+
+
+
 
 int main() {
-    
+    mat_mul(A, B);
     return 0;
 }
--- gemm.py
+++ gemm.py
@@ -2,16 +2,18 @@ import argparse
 import numpy as np
 import time
 
-N = 2 ** 10 # 1024
+N = 2 ** 4 # 16
 FLOPS = 2 * N * N * N
 
 
 def save_to_file(mat: np.array, file_name: str, premissions: str, write_to_file: bool):
     if not write_to_file:
+        print(f"write_to_file is set to: {write_to_file}, will not write to file: {file_name}")
         return
+    
     with open(file_name, premissions) as f:
         for line in mat:
-            np.savetxt(f, line, fmt='%.2f')
+            np.savetxt(f, line, fmt='%.2f')    
     
 
 def parse_args():
@@ -22,14 +24,14 @@ def parse_args():
     return args
 
 def main(write_to_file=True):
-    A = np.random.rand(N,N) # NxN matrix with values in range [0,1)
-    B = np.random.rand(N,N) # NxN matrix with values in range [0,1)
-    save_to_file(A, "A.txt", "w", write_to_file=write_to_file)
-    save_to_file(B, "B.txt", "w", write_to_file=write_to_file)
+    A = np.matrix(np.random.rand(N,N)) # NxN matrix with values in range [0,1)
+    B = np.matrix(np.random.rand(N,N)) # NxN matrix with values in range [0,1)
+    save_to_file(A, "A.txt", "wb", write_to_file=write_to_file)
+    save_to_file(B, "B.txt", "wb", write_to_file=write_to_file)
     st = time.monotonic()
     C = A @ B # multiply A with B
     en = time.monotonic()
-    save_to_file(C, "C.txt", "w", write_to_file=write_to_file) 
+    save_to_file(C, "C.txt", "wb", write_to_file=write_to_file) 
     exec_time = en-st
     print(f"Took {exec_time:.5f} seconds to multiply A with B")
     print(f"Number of GFLOPS: {FLOPS*1e-9/exec_time:.4f}")
