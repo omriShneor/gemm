Fixing int overflow with large values of N and changing the order of the for loops for better use of local memory - 0.88 GFLOP/S

gemm.py
@@ -2,7 +2,7 @@ import argparse
 import numpy as np
 import time
 
-N = 2 ** 4 # 16
+N = 2 ** 10 # 16
 FLOPS = 2 * N * N * N
 
 
@@ -13,7 +13,7 @@ def save_to_file(mat: np.array, file_name: str, premissions: str, write_to_file:
     
     with open(file_name, premissions) as f:
         for line in mat:
-            np.savetxt(f, line, fmt='%.2f')    
+            np.savetxt(f, line, fmt='%.5f')    
     
 
 def parse_args():
@@ -34,7 +34,7 @@ def main(write_to_file=True):
     save_to_file(C, "C.dat", "wb", write_to_file=write_to_file) 
     exec_time = en-st
     print(f"Took {exec_time:.5f} seconds to multiply A with B")
-    print(f"Number of GFLOPS: {FLOPS*1e-9/exec_time:.4f}")
+    print(f"Number of GFLOP/S with matmul in python: {FLOPS*1e-9/exec_time:.4f}")
 
 
 